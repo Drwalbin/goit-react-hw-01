733e0aa280de7985a9f08c81cf70db159df99e73
Sławomir Albiński <slawomiralbinski@gmail.com>
2022-10-17 19:33:58 +0200

    poprawki

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import './index.css';
 import App from './components/App';
 
 import Profile from 'components/Profile/Profile';
-import user from './components/Profile/user.json';
-
 import Statistics from './components/Statistics/Statistics';
-import data from './components/Statistics/data.json';
-
 import FriendList from './components/FriendList/FriendList';
-import friends from './components/FriendList/friends.json';
-
 import Transactions from './components/Transactions/Transactions.jsx';
+
+import user from './components/Profile/user.json';
+import data from './components/Statistics/data.json';
+import friends from './components/FriendList/friends.json';
 import history from './components/Transactions/transactions.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
